369c6ebfd66b309897681aa592aee2d75c77bc55
Prajwal <prajwalsoni>
2023-08-31 15:57:52 +0530

    remove comments

--- src/studentmanagement/StudentManagementApp.java
+++ src/studentmanagement/StudentManagementApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class StudentManagementApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StudentManagementSystem sms = new StudentManagementSystem(); //class
+        StudentManagementSystem sms = new StudentManagementSystem(); //class creation
 
         while (true) {
             System.out.println("Student Management System");
